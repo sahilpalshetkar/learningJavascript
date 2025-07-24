617e6b239dea5755dde02a8bff729caa714899a3
sahilpalshetkar <sahilpalshetkar125@gmail.com>
2025-07-24 23:32:00 +0530

    initial commit

---
+++ Callbacks&Promises/script.js
@@ -0,0 +1,12 @@
+console.log("Sahil is a hacker")
+console.log("Pratik is a hecker")
+
+setTimeout(() => {
+    console.log("I am inside setTimeout")
+}, 2000);
+
+setTimeout(() => {
+    console.log("I am inside setTimeout 2")
+}, 2000);
+
+console.log("The End!")
