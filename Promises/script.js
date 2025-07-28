58d60cccbd4cd21277cab8090456704e87e31e2d
sahilpalshetkar <sahilpalshetkar125@gmail.com>
2025-07-28 22:58:32 +0530

    promises

--- Promises/script.js
+++ Promises/script.js
@@ -2,11 +2,11 @@ console.log('This is promises');
 
 let prom1 = new Promise((resolve, reject)=>{
     let a = Math.random()
-    if (a<0.111) {
+    if (a<0.5) {
         reject("No random number was not supporting you")
     }
     else{
-    
+        
     setTimeout(() => {
         console.log("Yes, I am done")
         resolve("Sahil")
@@ -14,6 +14,23 @@ let prom1 = new Promise((resolve, reject)=>{
 }
 })
 
-prom1.then((a)=>{
+let prom2 = new Promise((resolve, reject)=>{
+    let a = Math.random()
+    if (a<0.5) {
+        reject("No random number was not supporting you2")
+    }
+    else{
+    
+    setTimeout(() => {
+        console.log("Yes, I am done2")
+        resolve("Sahil2")
+    }, 2000);
+}
+})
+
+let p3 = Promise.all([prom1,prom2])
+p3.then((a)=>{
     console.log(a)
+}).catch((err)=>{
+    console.log(err)
 })
