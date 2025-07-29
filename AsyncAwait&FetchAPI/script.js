c825576c22b6267088e08d212d538f9402bd4063
sahilpalshetkar <sahilpalshetkar125@gmail.com>
2025-07-29 17:29:53 +0530

    async functions learning

--- AsyncAwait&FetchAPI/script.js
+++ AsyncAwait&FetchAPI/script.js
@@ -42,3 +42,33 @@ async function main2() {
     console.log(data2)
 }
 main2()
+
+/*<------------------------------->*/
+async function sahil() {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve("Hey Sahil!");
+        }, 4500);
+    })
+}
+
+async function main3() {
+    let data3 = await sahil()
+    console.log(data3)
+}
+main3()
+
+/*<------------------------------->*/
+async function Sahil2() {
+    return new Promise((resolve, reject)=>{
+        setInterval(() => {
+            resolve("You are a goat!")
+        }, 5000);
+    })
+}
+
+async function main4() {
+    let s = await Sahil2()
+    console.log(s)
+}
+main4()
