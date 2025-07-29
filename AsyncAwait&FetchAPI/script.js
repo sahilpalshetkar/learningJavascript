621da6c9df67fb106a2b6db5b27a9756b38dd91e
sahilpalshetkar <sahilpalshetkar125@gmail.com>
2025-07-29 18:26:31 +0530

    commit

--- AsyncAwait&FetchAPI/script.js
+++ AsyncAwait&FetchAPI/script.js
@@ -6,10 +6,20 @@
 //     })
 // }
 async function getData(){  
-  let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-  let data = await x.json()
-  console.log(data)
-  return 455
+  let x = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+
+let data5 = await x.json()
+return data5
 }
 
 async function main() {
