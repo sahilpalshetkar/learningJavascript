8a84b2031981d36216a6559317f1cfd566f49a1e
sahilpalshetkar <sahilpalshetkar125@gmail.com>
2025-07-29 18:11:19 +0530

    commit

--- AsyncAwait&FetchAPI/script.js
+++ AsyncAwait&FetchAPI/script.js
@@ -1,9 +1,15 @@
+// async function getData(){  
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             resolve(455);
+//         }, 3500);
+//     })
+// }
 async function getData(){  
-    return new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            resolve(455);
-        }, 3500);
-    })
+  let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  let data = await x.json()
+  console.log(data)
+  return 455
 }
 
 async function main() {
