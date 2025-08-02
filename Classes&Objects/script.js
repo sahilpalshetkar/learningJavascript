1beee787a9ebc6c14b6e6c26a7c661c74ec32e1b
sahilpalshetkar <sahilpalshetkar125@gmail.com>
2025-08-02 13:29:57 +0530

    commit

--- Classes&Objects/script.js
+++ Classes&Objects/script.js
@@ -4,6 +4,8 @@
 // }
 // console.log(obj)
 
+// const { use } = require("react");
+
 // let animal = {
 //     eats:true
 // };
@@ -41,3 +43,28 @@ class Lion extends Animal{
 }
 let b = new Lion("sherr")
 console.log(b)
+
+// --------------------------------
+class User{
+    constructor(name){
+        //invokes the setter
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(value) {
+        if(value.length < 4) {
+            console.log("Name is too short.");
+            return;
+        }
+        this._name = value;
+    }
+}
+
+let user = new User("John");
+console.log(user.name)//John
+
+user.name = "" //Name is too
